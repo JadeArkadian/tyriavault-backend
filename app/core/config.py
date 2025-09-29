1e8f446c72869fcf3d58deccbd986b64e53f7394
Jade Arkadian <JadeArkadian@users.noreply.github.com>
2025-09-29 13:28:18 +0200

    Sets up basic FastAPI application structure
    Initializes the basic structure for a FastAPI application.
    
    This includes:
    
    - Project configuration with name and version.
    - API versioning.
    - A health check endpoint.
    - Basic requirements file.
    - Adds a run configuration for Uvicorn.
    - Ignores the .idea folder.

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+
+class Settings:
+    """
+    Configuration settings for the TyriaVault Backend application.
+    
+    """
+    PROJECT_NAME:str = "TyriaVault Backend ⚔️"
+    PROJECT_VERSION: str = "0.1.0"
+
+settings = Settings()
