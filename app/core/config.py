b5d48dcb46be04ea3d0aaad352caa205e1f00c4f
Hugo Cano Soriano <hugojulio.canosoriano@soprasteria.com>
2025-09-30 14:06:17 +0200

    Configures application settings via environment
    Configures the application to load settings from environment variables using pydantic-settings and dotenv. This allows for easier configuration management and deployment across different environments.
    Also adds account router for user authentication.

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings:
+load_dotenv()
+
+class Settings(BaseSettings):
     """
     Configuration settings for the TyriaVault Backend application.
 
@@ -8,5 +12,10 @@ class Settings:
     ## TODO: get these from pyproject.toml
     PROJECT_NAME:str = "TyriaVault Backend ⚔️"
     PROJECT_VERSION: str = "0.1.0"
+    DATABASE_URL: str
+    SECRET_KEY: str
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
 
 settings = Settings()
