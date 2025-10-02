9e86a0c4599e193728965d25c0839c332b677963
Jade Arkadian <hj.cano@alumno.ucjc.edu>
2025-10-02 23:21:43 +0200

    Adds initial item endpoint and DB setup
    Adds a basic item endpoint for demonstration purposes.
    Sets up database connection and table creation.
    
    This commit introduces the initial structure for handling
    items and configures the database to persist them.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
+
 from app.core.config import settings
 from app.api.v1 import api_router
+from app.db.session import engine
 
+# Setting up FastApi and our services
 api = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.PROJECT_VERSION,
 )
+api.include_router(api_router, prefix="/api/v1")
 
 print(settings.DATABASE_URL)
 
-# Include the API router with a versioned prefix
-api.include_router(api_router, prefix="/api/v1")
+# creates tables if not present
+from app.db.model import Base
+Base.metadata.create_all(bind=engine)
+
