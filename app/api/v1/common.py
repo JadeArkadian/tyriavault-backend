c7aec9acea6b5b504d1438481cce210572b698de
Jade Arkadian <JadeArkadian@users.noreply.github.com>
2025-10-07 17:04:55 +0200

    Adds API key authentication (#1)
    Implements an API key authentication system to secure endpoints.
    
    Validates API keys against the Guild Wars 2 API and caches them in the
    local database.
    Introduces a health check endpoint for service monitoring.
    
    Refactors database session management.
    Improves error handling and adds retry logic to the GW2 client.
    
    Co-authored-by: Hugo Cano Soriano <hugojulio.canosoriano@soprasteria.com>

--- app/api/v1/common.py
+++ app/api/v1/common.py
@@ -1,6 +1,14 @@
-from fastapi import APIRouter, Response
+import httpx
+from fastapi import APIRouter, Response, HTTPException
+from fastapi.params import Header, Depends
+from sqlalchemy.orm import Session
+
+from app.db.dependency import get_db
+from app.db.model import ApiKeys
+from app.gw2.client import GW2Client
 
 router = APIRouter(prefix="/common", tags=["common"])
+
 
 @router.get("/status", summary="Health Check", response_description="Service is alive")
 def status():
@@ -10,4 +18,79 @@ def status():
     - 200 OK with "alive" message if the service is running.
     - 500 Internal Server Error if the service is down.
     """
+
     return Response(content="alive", media_type="text/plain", status_code=200)
+
+
+@router.get("/tokeninfo", summary="Provides info about the API key", response_description="API Key info")
+def check_token_info(
+        authorization: str = Header(..., description="Authorization header: Bearer <API_KEY>"),
+        db: Session = Depends(get_db)):
+    """
+    Validates the provided API key and retrieves its information.
+    If the key is not found in the local database, it checks with the Guild Wars 2 API.
+    If valid, the key is stored in the local database for future requests.
+    :param authorization:
+    :param db:
+    :return:
+    - 200 OK with API key information if the key is valid.
+    - 400 Bad Request if the authorization header format is invalid.
+    - 401 Unauthorized if the API key is missing or invalid.
+    - 403 Forbidden if the API key is unauthorized.
+    - 503 Service Unavailable if there is a connection failure to the Guild Wars 2 API.
+    - 500 Internal Server Error for any other unexpected errors.
+    """
+
+    try:
+        token = _split_bearer_token(authorization)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    token_info = db.query(ApiKeys).filter(ApiKeys.api_key == token).first()
+
+    # No token found in DB -> check if it's valid with GW2 API
+    if token_info is None:
+        gw2 = GW2Client(api_key=token)
+        token_info = _get_token_info_from_api(gw2)
+        if token_info and token_info["permissions"] is not None:
+            # Store the valid token in the database
+            new_api_key = ApiKeys(
+                api_key=token,
+                permissions=token_info.get("permissions", []),
+                game_account_id=None
+            )
+
+            db.add(new_api_key)
+            db.commit()
+            db.refresh(new_api_key)
+            token_info = new_api_key
+
+    return token_info
+
+
+def _split_bearer_token(authorization: str) -> str:
+    try:
+        scheme, token = authorization.split()
+        if scheme.lower() != "bearer":
+            raise ValueError("Invalid authentication scheme")
+        return token
+    except ValueError:
+        raise ValueError("Invalid authorization header format")
+
+
+def _get_token_info_from_api(gw2: GW2Client):
+    try:
+        return gw2.token_info()
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 401:
+            raise HTTPException(status_code=401, detail="Missing or invalid token.")
+        elif e.response.status_code == 403:
+            raise HTTPException(status_code=403, detail="Missing or unauthorized token.")
+        else:
+            raise HTTPException(status_code=e.response.status_code, detail=e.response.text)
+
+    except httpx.RequestError as e:
+        raise HTTPException(status_code=503, detail=f"Conection failure: {str(e)}")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal error: {str(e)}")
--- app/db/model.py
+++ app/db/model.py
@@ -1,9 +1,11 @@
-from typing import Optional
 import datetime
+from typing import Optional
 
-from sqlalchemy import BigInteger, DateTime, ForeignKeyConstraint, Identity, Index, Integer, PrimaryKeyConstraint, String, Text, UniqueConstraint, text
+from sqlalchemy import BigInteger, DateTime, ForeignKeyConstraint, Identity, Index, Integer, PrimaryKeyConstraint, \
+    String, Text, UniqueConstraint, text
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
 
 class Base(DeclarativeBase):
     pass
@@ -22,7 +24,8 @@ class Achievements(Base):
     name_en: Mapped[str] = mapped_column(Text, nullable=False)
     name_de: Mapped[str] = mapped_column(Text, nullable=False)
     achievement_type: Mapped[str] = mapped_column(String(60), nullable=False)
-    last_fetched: Mapped[datetime.datetime] = mapped_column(DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+    last_fetched: Mapped[datetime.datetime] = mapped_column(DateTime(True), nullable=False,
+                                                            server_default=text('CURRENT_TIMESTAMP'))
     description_es: Mapped[Optional[str]] = mapped_column(Text)
     description_fr: Mapped[Optional[str]] = mapped_column(Text)
     description_en: Mapped[Optional[str]] = mapped_column(Text)
@@ -38,7 +41,9 @@ class Currencies(Base):
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(Integer, Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
     name_es: Mapped[Optional[str]] = mapped_column(String(100))
     name_fr: Mapped[Optional[str]] = mapped_column(String(100))
     name_en: Mapped[Optional[str]] = mapped_column(String(100))
@@ -56,7 +61,9 @@ class Emotes(Base):
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(Integer, Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
     command: Mapped[str] = mapped_column(String(50), nullable=False)
 
     unlocked_emotes: Mapped[list['UnlockedEmotes']] = relationship('UnlockedEmotes', back_populates='emote')
@@ -85,7 +92,9 @@ class ItemTypes(Base):
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(Integer, Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
     name_es: Mapped[str] = mapped_column(String(100), nullable=False)
     name_fr: Mapped[str] = mapped_column(String(100), nullable=False)
     name_en: Mapped[str] = mapped_column(String(100), nullable=False)
@@ -101,7 +110,9 @@ class Professions(Base):
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(Integer, Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
     name_es: Mapped[str] = mapped_column(String(100), nullable=False)
     name_fr: Mapped[str] = mapped_column(String(100), nullable=False)
     name_en: Mapped[str] = mapped_column(String(100), nullable=False)
@@ -117,7 +128,9 @@ class Races(Base):
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(Integer, Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
     name_es: Mapped[str] = mapped_column(String(100), nullable=False)
     name_fr: Mapped[str] = mapped_column(String(100), nullable=False)
     name_en: Mapped[str] = mapped_column(String(100), nullable=False)
@@ -133,7 +146,9 @@ class Rarities(Base):
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(Integer, Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
     name_es: Mapped[str] = mapped_column(String(100), nullable=False)
     name_fr: Mapped[str] = mapped_column(String(100), nullable=False)
     name_en: Mapped[str] = mapped_column(String(100), nullable=False)
@@ -161,19 +176,25 @@ class Worlds(Base):
 class GameAccounts(Base):
     __tablename__ = 'game_accounts'
     __table_args__ = (
-        ForeignKeyConstraint(['world_id'], ['schema_tyriavault.worlds.id'], onupdate='CASCADE', name='fk_game_accounts_worlds'),
+        ForeignKeyConstraint(['world_id'], ['schema_tyriavault.worlds.id'], onupdate='CASCADE',
+                             name='fk_game_accounts_worlds'),
         PrimaryKeyConstraint('id', name='pk_game_accounts'),
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(Integer, Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
     account_name: Mapped[str] = mapped_column(String(80), nullable=False)
     world_id: Mapped[int] = mapped_column(Integer, nullable=False, server_default=text('0'))
-    creation_date: Mapped[datetime.datetime] = mapped_column(DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+    creation_date: Mapped[datetime.datetime] = mapped_column(DateTime(True), nullable=False,
+                                                             server_default=text('CURRENT_TIMESTAMP'))
     fractal_level: Mapped[int] = mapped_column(Integer, nullable=False, server_default=text('1'))
-    last_modified: Mapped[datetime.datetime] = mapped_column(DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+    last_modified: Mapped[datetime.datetime] = mapped_column(DateTime(True), nullable=False,
+                                                             server_default=text('CURRENT_TIMESTAMP'))
 
     world: Mapped['Worlds'] = relationship('Worlds', back_populates='game_accounts')
+    api_keys: Mapped[list['ApiKeys']] = relationship('ApiKeys', back_populates='game_account')
     characters: Mapped[list['Characters']] = relationship('Characters', back_populates='game_account')
     unlocked_emotes: Mapped[list['UnlockedEmotes']] = relationship('UnlockedEmotes', back_populates='game_account')
     wallet: Mapped[list['Wallet']] = relationship('Wallet', back_populates='game_account')
@@ -183,8 +204,10 @@ class GameAccounts(Base):
 class ItemsCache(Base):
     __tablename__ = 'items_cache'
     __table_args__ = (
-        ForeignKeyConstraint(['item_type_id'], ['schema_tyriavault.item_types.id'], ondelete='CASCADE', onupdate='CASCADE', name='fk_items_cache_item_type'),
-        ForeignKeyConstraint(['rarity_id'], ['schema_tyriavault.rarities.id'], ondelete='CASCADE', onupdate='CASCADE', name='fk_items_cache_rarities'),
+        ForeignKeyConstraint(['item_type_id'], ['schema_tyriavault.item_types.id'], ondelete='CASCADE',
+                             onupdate='CASCADE', name='fk_items_cache_item_type'),
+        ForeignKeyConstraint(['rarity_id'], ['schema_tyriavault.rarities.id'], ondelete='CASCADE', onupdate='CASCADE',
+                             name='fk_items_cache_rarities'),
         PrimaryKeyConstraint('id', name='pk_items_cache'),
         Index('idx_items_cache_0', 'item_type_id'),
         Index('idx_items_cache_1', 'rarity_id'),
@@ -198,7 +221,8 @@ class ItemsCache(Base):
     name_de: Mapped[str] = mapped_column(String(200), nullable=False)
     item_type_id: Mapped[int] = mapped_column(Integer, nullable=False)
     rarity_id: Mapped[int] = mapped_column(Integer, nullable=False)
-    last_fetched: Mapped[datetime.datetime] = mapped_column(DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+    last_fetched: Mapped[datetime.datetime] = mapped_column(DateTime(True), nullable=False,
+                                                            server_default=text('CURRENT_TIMESTAMP'))
     icon: Mapped[Optional[str]] = mapped_column(Text)
     item_level: Mapped[Optional[int]] = mapped_column(Integer)
     vendor_value: Mapped[Optional[int]] = mapped_column(Integer, server_default=text('0'))
@@ -208,20 +232,47 @@ class ItemsCache(Base):
     rarity: Mapped['Rarities'] = relationship('Rarities', back_populates='items_cache')
 
 
+class ApiKeys(Base):
+    __tablename__ = 'api_keys'
+    __table_args__ = (
+        ForeignKeyConstraint(['game_account_id'], ['schema_tyriavault.game_accounts.id'],
+                             name='fk_api_keys_game_accounts'),
+        PrimaryKeyConstraint('id', name='pk_api_keys'),
+        Index('idx_api_keys', 'game_account_id'),
+        Index('unq_api_keys', 'api_key', unique=True),
+        {'schema': 'schema_tyriavault'}
+    )
+
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
+    api_key: Mapped[str] = mapped_column(String(80), nullable=False)
+    permissions: Mapped[Optional[dict]] = mapped_column(JSONB)
+    game_account_id: Mapped[Optional[int]] = mapped_column(Integer)
+
+    game_account: Mapped[Optional['GameAccounts']] = relationship('GameAccounts', back_populates='api_keys')
+
+
 class Characters(Base):
     __tablename__ = 'characters'
     __table_args__ = (
-        ForeignKeyConstraint(['game_account_id'], ['schema_tyriavault.game_accounts.id'], name='fk_characters_game_accounts'),
-        ForeignKeyConstraint(['gender_id'], ['schema_tyriavault.genders.id'], ondelete='CASCADE', onupdate='CASCADE', name='fk_characters_genders'),
-        ForeignKeyConstraint(['profession_id'], ['schema_tyriavault.professions.id'], ondelete='CASCADE', onupdate='CASCADE', name='fk_characters_professions'),
-        ForeignKeyConstraint(['race_id'], ['schema_tyriavault.races.id'], ondelete='CASCADE', onupdate='CASCADE', name='fk_characters_races'),
+        ForeignKeyConstraint(['game_account_id'], ['schema_tyriavault.game_accounts.id'],
+                             name='fk_characters_game_accounts'),
+        ForeignKeyConstraint(['gender_id'], ['schema_tyriavault.genders.id'], ondelete='CASCADE', onupdate='CASCADE',
+                             name='fk_characters_genders'),
+        ForeignKeyConstraint(['profession_id'], ['schema_tyriavault.professions.id'], ondelete='CASCADE',
+                             onupdate='CASCADE', name='fk_characters_professions'),
+        ForeignKeyConstraint(['race_id'], ['schema_tyriavault.races.id'], ondelete='CASCADE', onupdate='CASCADE',
+                             name='fk_characters_races'),
         PrimaryKeyConstraint('id', name='pk_characters'),
         UniqueConstraint('name', name='unq_name_characters'),
         Index('idx_characters', 'game_account_id'),
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(Integer, Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
     game_account_id: Mapped[int] = mapped_column(Integer, nullable=False)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     race_id: Mapped[int] = mapped_column(Integer, nullable=False)
@@ -238,7 +289,8 @@ class Characters(Base):
 class ItemDetails(ItemsCache):
     __tablename__ = 'item_details'
     __table_args__ = (
-        ForeignKeyConstraint(['item_id'], ['schema_tyriavault.items_cache.id'], ondelete='CASCADE', onupdate='CASCADE', name='fk_item_details_items_cache'),
+        ForeignKeyConstraint(['item_id'], ['schema_tyriavault.items_cache.id'], ondelete='CASCADE', onupdate='CASCADE',
+                             name='fk_item_details_items_cache'),
         PrimaryKeyConstraint('item_id', name='pk_item_details'),
         Index('idx_item_details', 'details'),
         {'schema': 'schema_tyriavault'}
@@ -252,14 +304,17 @@ class UnlockedEmotes(Base):
     __tablename__ = 'unlocked_emotes'
     __table_args__ = (
         ForeignKeyConstraint(['emote_id'], ['schema_tyriavault.emotes.id'], name='fk_unlocked_emotes_emotes'),
-        ForeignKeyConstraint(['game_account_id'], ['schema_tyriavault.game_accounts.id'], name='fk_unlocked_emotes_game_accounts'),
+        ForeignKeyConstraint(['game_account_id'], ['schema_tyriavault.game_accounts.id'],
+                             name='fk_unlocked_emotes_game_accounts'),
         PrimaryKeyConstraint('id', name='pk_emotes'),
         UniqueConstraint('game_account_id', 'emote_id', name='unq_unlocked_emotes'),
         Index('idx_unlocked_emotes', 'game_account_id'),
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(Integer, Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
     game_account_id: Mapped[int] = mapped_column(Integer, nullable=False)
     emote_id: Mapped[int] = mapped_column(Integer, nullable=False)
 
@@ -270,15 +325,19 @@ class UnlockedEmotes(Base):
 class Wallet(Base):
     __tablename__ = 'wallet'
     __table_args__ = (
-        ForeignKeyConstraint(['currency_id'], ['schema_tyriavault.currencies.id'], ondelete='CASCADE', onupdate='CASCADE', name='fk_wallet_currencies'),
-        ForeignKeyConstraint(['game_account_id'], ['schema_tyriavault.game_accounts.id'], ondelete='CASCADE', onupdate='CASCADE', name='fk_wallet_game_accounts'),
+        ForeignKeyConstraint(['currency_id'], ['schema_tyriavault.currencies.id'], ondelete='CASCADE',
+                             onupdate='CASCADE', name='fk_wallet_currencies'),
+        ForeignKeyConstraint(['game_account_id'], ['schema_tyriavault.game_accounts.id'], ondelete='CASCADE',
+                             onupdate='CASCADE', name='fk_wallet_game_accounts'),
         PrimaryKeyConstraint('id', name='pk_wallet'),
         UniqueConstraint('currency_id', 'game_account_id', name='unq_wallet'),
         Index('idx_wallet', 'game_account_id'),
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(Integer, Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,
+                                    Identity(start=0, increment=1, minvalue=0, maxvalue=2147483647, cycle=False,
+                                             cache=1), primary_key=True)
     currency_id: Mapped[int] = mapped_column(Integer, nullable=False, server_default=text('0'))
     game_account_id: Mapped[int] = mapped_column(Integer, nullable=False)
     amount: Mapped[Optional[int]] = mapped_column(Integer, server_default=text('0'))
@@ -291,17 +350,21 @@ class WalletHistory(Base):
     __tablename__ = 'wallet_history'
     __table_args__ = (
         ForeignKeyConstraint(['currency_id'], ['schema_tyriavault.currencies.id'], name='fk_wallet_history_currencies'),
-        ForeignKeyConstraint(['game_account_id'], ['schema_tyriavault.game_accounts.id'], name='fk_wallet_history_game_accounts'),
+        ForeignKeyConstraint(['game_account_id'], ['schema_tyriavault.game_accounts.id'],
+                             name='fk_wallet_history_game_accounts'),
         PrimaryKeyConstraint('id', name='pk_wallet_history'),
         Index('idx_wallet_history', 'game_account_id'),
         Index('unq_wallet_history', 'game_account_id', 'currency_id', 'snapshot_time', unique=True),
         {'schema': 'schema_tyriavault'}
     )
 
-    id: Mapped[int] = mapped_column(BigInteger, Identity(always=True, start=0, increment=1, minvalue=0, maxvalue=9223372036854775807, cycle=False, cache=1), primary_key=True)
+    id: Mapped[int] = mapped_column(BigInteger, Identity(always=True, start=0, increment=1, minvalue=0,
+                                                         maxvalue=9223372036854775807, cycle=False, cache=1),
+                                    primary_key=True)
     currency_id: Mapped[int] = mapped_column(Integer, nullable=False)
     game_account_id: Mapped[int] = mapped_column(Integer, nullable=False)
-    snapshot_time: Mapped[datetime.datetime] = mapped_column(DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+    snapshot_time: Mapped[datetime.datetime] = mapped_column(DateTime(True), nullable=False,
+                                                             server_default=text('CURRENT_TIMESTAMP'))
     amount: Mapped[Optional[int]] = mapped_column(Integer)
 
     currency: Mapped['Currencies'] = relationship('Currencies', back_populates='wallet_history')
