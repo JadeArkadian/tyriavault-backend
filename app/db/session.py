054d8068cc3e022a13d2a61ec301acc256dfb6b5
Hugo Cano Soriano <hugojulio.canosoriano@soprasteria.com>
2025-10-07 17:03:25 +0200

    Adds API key authentication
    Implements an API key authentication system to secure endpoints.
    
    Validates API keys against the Guild Wars 2 API and caches them in the local database.
    Introduces a health check endpoint for service monitoring.
    
    Refactors database session management.
    Improves error handling and adds retry logic to the GW2 client.

--- app/db/session.py
+++ app/db/session.py
@@ -1,16 +1,13 @@
-from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core import settings
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+from app.core import settings
 
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10,
+)
 
-def get_db():
-    """Dependency that provides a database session."""
-
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from app.api.v1 import api_router
 from app.core.config import settings
 from app.db.model import Base
 from app.db.session import engine
-from app.gw2.client import GW2Client
 
 # Setting up FastApi and our services
 api = FastAPI(
@@ -13,12 +12,13 @@ api = FastAPI(
 )
 api.include_router(api_router, prefix="/api/v1")
 
+
 @api.on_event("startup")
 def startup_event():
     print("Turn On")
     print(settings.DATABASE_URL)
     # creates tables if not present
     Base.metadata.create_all(bind=engine)
-    gw2 = GW2Client(api_key=settings.GW2_API_KEY)
-    test = gw2.get_account()
-    print(test)
+    # gw2 = GW2Client(api_key=settings.GW2_API_KEY)
+    # test = gw2.get_account()
+    # print(test)
