9fa7f039811d17031e5eb8c7048f3001989e5bc1
Hugo Cano Soriano <hugojulio.canosoriano@soprasteria.com>
2025-10-09 22:31:52 +0200

    Worlds crawler

---
+++ app/crawlers/worlds_crawler.py
@@ -0,0 +1,46 @@
+from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.api.v1.worlds import get_worlds_info_from_api
+from app.db.model import Worlds
+from app.gw2.client import GW2Client
+
+
+async def update_worlds_incremental(db: AsyncSession):
+    gw2 = GW2Client()
+    print("Start updating worlds incremental")
+
+    # Query worlds from GW2 API
+    worlds_info_from_api = await get_worlds_info_from_api(gw2)
+    if not worlds_info_from_api:
+        return
+
+    # Check existing worlds in DB
+    result = await db.execute(select(Worlds))
+    worlds_db = {w.id: w for w in result.scalars().all()}
+
+    # Update or insert worlds as necessary
+    for world in worlds_info_from_api:
+        world_id = world["id"]
+        names = {
+            "name_es": world.get("name_es", ""),
+            "name_fr": world.get("name_fr", ""),
+            "name_en": world.get("name_en", ""),
+            "name_de": world.get("name_de", "")
+        }
+        if world_id in worlds_db:
+            db_world = worlds_db[world_id]
+            # If any name has changed, update the record
+            if any(getattr(db_world, k) != v for k, v in names.items()):
+                print(f"Updating world: {world_id}")
+                await db.execute(
+                    update(Worlds)
+                    .where(Worlds.id == world_id)
+                    .values(**names)
+                )
+        else:
+            # Insert new world
+            print(f"inserting new world: {world_id}")
+            db.add(Worlds(id=world_id, **names))
+    await db.commit()
+    print("End updating worlds incremental")
--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
+import asyncio
 from contextlib import asynccontextmanager
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1 import api_router
 from app.core.config import settings
+from app.crawlers.worlds_crawler import update_worlds_incremental
+from app.db.dependency import get_db
 from app.db.model import Base
 from app.db.session import engine
 from app.gw2.client import startup_gw2_client, shutdown_gw2_client
@@ -18,10 +22,43 @@ async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     await startup_gw2_client()
+
+    # execute the worlds crawler once at startup
+    await run_worlds_crawler_startup()
+
+    # Schedule the worlds crawler to run daily at 5:00 AM
+    scheduler = schedule_worlds_crawler_daily()
+
     yield
     # Shutdown
     print("Turn Off")
     await shutdown_gw2_client()
+    scheduler.shutdown()
+
+
+def run_worlds_crawler_startup():
+    async def _run():
+        async for db in get_db():
+            await update_worlds_incremental(db)
+            break
+
+    return _run()
+
+
+def schedule_worlds_crawler_daily():
+    scheduler = AsyncIOScheduler()
+
+    def schedule_crawler():
+        async def run():
+            async for db in get_db():
+                await update_worlds_incremental(db)
+                break
+
+        asyncio.create_task(run())
+
+    scheduler.add_job(schedule_crawler, 'cron', hour=22, minute=29)
+    scheduler.start()
+    return scheduler
 
 
 # Setting up FastApi and our services
