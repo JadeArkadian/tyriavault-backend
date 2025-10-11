eec2639c16c99c04192ee66efc8bd43825227a43
Jade Arkadian <JadeArkadian@users.noreply.github.com>
2025-10-11 17:16:38 +0200

    Seeding mechanism setup for some tables and model update (#21)
    Co-authored-by: Jade Arkadian <hj.cano@alumno.ucjc.edu>

---
+++ app/db/data/professions_data.py
@@ -0,0 +1,11 @@
+PROFESSIONS_DATA = [
+    {"id": 0, "name_en": "Guardian", "name_es": "Guardián", "name_de": "Wächter", "name_fr": "Gardien"},
+    {"id": 1, "name_en": "Warrior", "name_es": "Guerrero", "name_de": "Krieger", "name_fr": "Guerrier"},
+    {"id": 2, "name_en": "Engineer", "name_es": "Ingeniero", "name_de": "Ingenieur", "name_fr": "Ingénieur"},
+    {"id": 3, "name_en": "Ranger", "name_es": "Guardabosques", "name_de": "Waldläufer", "name_fr": "Rôdeur"},
+    {"id": 4, "name_en": "Thief", "name_es": "Ladrón", "name_de": "Dieb", "name_fr": "Voleur"},
+    {"id": 5, "name_en": "Elementalist", "name_es": "Elementalista", "name_de": "Elementarmagier", "name_fr": "Élémentaliste"},
+    {"id": 6, "name_en": "Mesmer", "name_es": "Hipnotizador", "name_de": "Mesmer", "name_fr": "Envoûteur"},
+    {"id": 7, "name_en": "Necromancer", "name_es": "Nigromante", "name_de": "Nekromant", "name_fr": "Nécromant"},
+    {"id": 8, "name_en": "Revenant", "name_es": "Retornado", "name_de": "Widergänger", "name_fr": "Revenant"},
+]
---
+++ app/db/data/races_data.py
@@ -0,0 +1,7 @@
+RACES_DATA = [
+    {"id": 1, "name_en": "Asura", "name_es": "Asura", "name_de": "Asura", "name_fr": "Asura"},
+    {"id": 2, "name_en": "Charr", "name_es": "Charr", "name_de": "Charr", "name_fr": "Charr"},
+    {"id": 3, "name_en": "Human", "name_es": "Humano", "name_de": "Mensch", "name_fr": "Humain"},
+    {"id": 4, "name_en": "Norn", "name_es": "Norn", "name_de": "Norn", "name_fr": "Norn"},
+    {"id": 5, "name_en": "Sylvari", "name_es": "Sylvari", "name_de": "Sylvari", "name_fr": "Sylvari"},
+]
