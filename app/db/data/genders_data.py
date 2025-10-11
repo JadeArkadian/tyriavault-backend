978ca30e2c6b52e58ea2f2713377df18a3146acc
Jade Arkadian <hj.cano@alumno.ucjc.edu>
2025-10-11 16:14:46 +0200

    Seeding of almost invariable character data (race, profession and gender)

---
+++ app/db/data/genders_data.py
@@ -0,0 +1,4 @@
+GENDERS_DATA = [
+    {"id": 1, "name_en": "Male", "name_es": "Masculino", "name_de": "Männlich", "name_fr": "Homme"},
+    {"id": 2, "name_en": "Female", "name_es": "Femenino", "name_de": "Weiblich", "name_fr": "Femme"},
+]
