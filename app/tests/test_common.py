ddee7c8c13bab562ed0436531bfb35901e94bf5d
Hugo Cano Soriano <hugojulio.canosoriano@soprasteria.com>
2025-10-15 17:36:46 +0200

    Test fix

--- app/tests/test_common.py
+++ app/tests/test_common.py
@@ -30,6 +30,8 @@ async def test_check_token_info_token_in_db():
     with patch("app.core.utils.split_bearer_token", return_value=mock_token):
         result = await check_token_info(authorization="Bearer valid_token", db=mock_db)
         assert result == mock_api_key
+        assert result.permissions == mock_api_key.permissions
+        assert result.game_account_uuid == mock_api_key.game_account_uuid
 
 
 # Test para check_token_info: token no está en la base de datos, pero es válido en GW2 API
