76e216cc3ab675c1697a78438e6841167c5eefd2
Jade Arkadian <viento2k6@gmail.com>
2025-10-31 03:37:21 +0100

    Item model WIP

--- app/gw2/responses/__init__.py
+++ app/gw2/responses/__init__.py
@@ -1,9 +1,11 @@
 """
 GW2 API response models.
 """
+
 from app.gw2.responses.gw2api_account import GW2ApiAccount
 from app.gw2.responses.gw2api_colors import GW2ApiColor, MaterialColor
 from app.gw2.responses.gw2api_currencies import GW2ApiCurrency
+from app.gw2.responses.gw2api_items import GW2ApiItem
 from app.gw2.responses.gw2api_tokeninfo import GW2ApiTokenInfo
 from app.gw2.responses.gw2api_wallet import GW2ApiWalletEntry
 from app.gw2.responses.gw2api_worlds import GW2ApiWorld
@@ -11,9 +13,10 @@ from app.gw2.responses.gw2api_worlds import GW2ApiWorld
 __all__ = [
     "GW2ApiAccount",
     "GW2ApiColor",
-    "MaterialColor",
     "GW2ApiCurrency",
+    "GW2ApiItem",
     "GW2ApiTokenInfo",
     "GW2ApiWalletEntry",
     "GW2ApiWorld",
+    "MaterialColor"
 ]
